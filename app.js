fd3659d0312c62e20aaa095ea4bdcdcbea22030d
Edmond Hui <edmond.t.hui@gmail.com>
2021-02-23 00:07:50 -0500

    start working on this app again

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 8000; // process.env accesses heroku's environment variables
-app.use(express.static("public"));
+app.use(express.static(`${__dirname}/public`));
 
-app.get("/", (request, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+// app.get("/", (request, res) => {
+//   res.sendFile(path.join(__dirname, "./public/index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(__dirname);
