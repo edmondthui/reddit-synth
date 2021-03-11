b056c98e48829e7104c312c1c38c01a77840cf50
Edmond Hui <edmond.t.hui@gmail.com>
2021-03-10 21:04:35 -0500

    add meta description

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const path = require("path");
 const PORT = process.env.PORT || 8000; // process.env accesses heroku's environment variables
 app.use(express.static(`${__dirname}/public`));
 
-// app.get("/", (request, res) => {
-//   res.sendFile(path.join(__dirname, "./public/index.html"));
-// });
+app.get("/image", (request, res) => {
+  res.sendFile(
+    path.join(__dirname, "./public/assets/reddit_synth_og_image.gif")
+  );
+});
 
 app.listen(PORT, () => {
   console.log(__dirname);
